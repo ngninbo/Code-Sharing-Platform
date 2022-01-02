19c9ee7ebdd1189a53090d86fcc17bfd2692f175
Beauclair Ngnintedem <beauclair.dongmo.ngnintedem@cofinpro.de>
2022-01-02 19:11:40 +0100

    CHG: Refactored code

--- Code Sharing Platform/task/src/platform/utils/CodeSnippetDateFormatter.java
+++ Code Sharing Platform/task/src/platform/utils/CodeSnippetDateFormatter.java
@@ -7,8 +7,9 @@ public class CodeSnippetDateFormatter {
 
     private static final String DATE_FORMATTER= "yyyy-MM-dd HH:mm:ss";
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+
     public static String formatDate(LocalDateTime date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
-        return formatter.format(date);
+        return FORMATTER.format(date);
     }
 }
